f99285773fa699913f643d809f0f1d0907598f3f
melenadesign <lexymer@gmail.com>
2019-07-19 23:03:03 +0300

    parallelogram init added

--- Task_2/index.js
+++ Task_2/index.js
@@ -1,5 +1,62 @@
 function calcParal() {
-    console.log('Parallelogram In Progress')
+  let sideA = parseFloat(document.getElementById('parallelogramSideA').value);
+  let sideB = parseFloat(document.getElementById('parallelogramSideB').value);
+  let angle = Math.abs(parseFloat(document.getElementById('parallelogramAngle').value));
+
+  const angle0 = 0;
+  const angle180 = 180;
+  const angle90 = 90;
+  const numAccTo = 3;
+  let angleSkewToFigure = angle90 - angle;
+  let angleDegToRad = angleSkewToFigure * Math.PI / angle180;
+  let maxFig = 200;
+  let parallelogramArea = 0;
+  let parallelogramHeight = 0;
+
+
+
+  if(angle === angle90) {
+    parallelogramArea = Math.abs(sideA * sideB).toFixed(numAccTo);
+    parallelogramHeight = Math.abs(sideB.toFixed(numAccTo));
+  } else {
+    parallelogramArea = Math.abs(sideA * sideB * Math.sin(angleDegToRad)).toFixed(numAccTo);
+    parallelogramHeight = Math.abs((parallelogramArea / sideB).toFixed(numAccTo));
+  }
+
+  if (angle === angle0 || angle === angle180 || isNaN(parallelogramArea) || isNaN(parallelogramHeight) ) {
+    parallelogramArea = '-';
+    parallelogramHeight = '-';
+  }
+  document.getElementById('parallelogram_Area').innerHTML = parallelogramArea;
+  document.getElementById('parallelogram_Height').innerHTML = parallelogramHeight;
+
+  let figParal = document.getElementById('parallelogram_img');
+  const smallFigCoef = 0.4;
+  const middleFigCoef = 0.7;
+  const smallFigAngle = 10;
+  const middleFigAngle = 35;
+
+  let scale = 1;
+
+  if( angle < smallFigAngle ) {
+    scale = smallFigCoef;
+  } else if (angle < middleFigAngle) {
+    scale = middleFigCoef;
+  }
+
+  if( sideA > sideB && sideA > maxFig) { 
+    sideB *= maxFig / sideA;
+    sideA = maxFig;
+    scale = maxFig / sideA;
+  } else if (sideB > sideA && sideB > maxFig) {
+    sideA *= maxFig / sideB;
+    sideB = maxFig;
+    scale = maxFig / sideB;
+  } 
+
+  figParal.style.width = sideA + 'px'; 
+  figParal.style.height = sideB + 'px';
+  figParal.style = `transform: matrix(${scale},0,${angleDegToRad},${scale},0,0)`;
 }
 
 function calcOval() {
