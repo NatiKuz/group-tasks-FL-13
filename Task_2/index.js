a9b8a5dbfe22158953b1be814c35ad7527b5b7b0
Alinadov <alinadovich@gmail.com>
2019-07-19 15:09:19 +0300

    add rectangle

--- Task_2/index.js
+++ Task_2/index.js
@@ -24,10 +24,33 @@ function calcSquare() {
     fig.style.height = a + 'px';
   }
 
+function calcRectangle() {
+    let a = parseFloat(document.getElementById('rectangleSideA').value);
+    let b = parseFloat(document.getElementById('rectangleSideB').value);  
+    let rectangleArea = a * b;
+    let rectangleDiagonal = Math.round(Math.sqrt(a * a + b * b));
+    document.getElementById('rectangle_Area').innerHTML = rectangleArea;
+    document.getElementById('rectangle_Diagonal').innerHTML = rectangleDiagonal;
+
+    let fig = document.getElementById('rectangle_img');
+
+
+    fig.style.width = a + 'px'; 
+    fig.style.height = b + 'px';
+
+  }
+
+function calcTriangle() {
+    console.log('Triangle In Progress')
+}
+
+
 
 function calcAll() {
     calcParal();
     calcOval();
     calcCircle();
     calcSquare();
+    calcRectangle()
+    calcTriangle()
   }
